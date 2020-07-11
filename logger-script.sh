b559676b7a743594a8820c1aa56dc9dd55777e17
arunmohan_fw <arunmohan@factweavers.com>
2020-07-11 22:02:39 +0530

    fomratting

--- logger-script.sh
+++ logger-script.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+# 2020-07-11T19:46:08+05:30 - INFO - DESKTOP-0SV07OJ - app-01 - 2 - logging into application 01
 i=0;
+timestamp=$(date --iso-8601=seconds);
+loglevel_01="INFO"
+loglevel_02="WARN"
+host=$(hostname)
+message_01="logging into application 01"
+message_02="logging into application 02"
+
 while true; do
-  echo "$i logging to my application $(date)" >> /var/log/my-app.log; i=$((i+1)); 
-  logger "loggin to syslog at $(date)"
+  echo "$timestamp - $loglevel_01 - $host - app-01 - $i - $message_01" >> /var/log/my-app-01.log; 
+  echo "$timestamp - $loglevel_02 - $host - app-02 - $i - $message_02" >> /var/log/my-app-02.log; 
+  i=$((i+1)); 
   sleep 1;
 done
