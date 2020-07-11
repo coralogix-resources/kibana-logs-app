d8a59cc0d550eedbeec3f18cedb8baf37ee7d38c
arunmohan_fw <arunmohan@factweavers.com>
2020-07-11 14:42:15 +0530

    editing script

--- logger-script.sh
+++ logger-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 i=0;
 while true; do
-  echo "$i logging to my application $(date)" >> /var/log/app.log; i=$((i+1)); 
+  echo "$i logging to my application $(date)" >> /var/log/my-app.log; i=$((i+1)); 
   logger "loggin to syslog at $(date)"
   sleep 1;
 done
